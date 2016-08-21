d74d816b0a38fab06a91a5d498ae1ccd5c3754ed
Markus Ånöstam <markus87@gmail.com>
2016-08-21 16:57:47 +0200

    Clean up

--- server.js
+++ server.js
@@ -21,34 +21,11 @@ const github = new GitHubApi({
 // Eslint configuration
 const eslint = new ESLintCLIEngine(eslintConfig);
 
-const filterJavascriptFiles = (files) =>
-    files.filter(({ filename }) => filename.match(process.env.FILE_FILTER));
+const env = (name) => process.env[name];
 
-const getContent = (file, ref, prNumber, sha) => {
-    const { filename, patch } = file;
-    // Todo: Can be rewritten without manually resolving or rejecting?
-    return new Promise((resolve, reject) => {
-        github.repos.getContent({
-            user: process.env.REPOSITORY_OWNER,
-            repo: process.env.REPOSITORY_NAME,
-            path: filename,
-            ref,
-        }, (error, data) => {
-            if (error) {
-                console.error(error);
-                reject(error);
-            } else {
-                resolve({
-                    filename,
-                    patch,
-                    sha,
-                    prNumber,
-                    content: atob(data.content),
-                });
-            }
-        });
-    });
-};
+function filterJavascriptFiles(files) {
+    return files.filter(({ filename }) => filename.match(env('FILE_FILTER')));
+}
 
 /**
  * Compute a mapping object for the relationship:
@@ -61,7 +38,7 @@ const getContent = (file, ref, prNumber, sha) => {
  * @param  {String}   patchString               The git patch string.
  * @return {Object} An object shaped as follows : {'file line number': 'diff view line number'}.
  */
-const getLineMapFromPatchString = (patchString) => {
+function getLineMapFromPatchString(patchString) {
     let diffLineIndex = 0;
     let fileLineIndex = 0;
     return patchString.split('\n').reduce((lineMap, line) => {
@@ -78,24 +55,7 @@ const getLineMapFromPatchString = (patchString) => {
         }
         return lineMap;
     }, {});
-};
-
-/**
- * Lint a raw content passed as a string, then return the linting messages.
- * @param  {String} filename File filename
- * @param  {String} patch    Commit's Git patch
- * @param  {String} content  File content
- * @param  {String} sha      Commit's id
- * @param  {Number} prNumber   Pull request number
- * @return {Array}  Linting messages
- */
-const lintContent = ({ filename, patch, content, sha, prNumber }) => ({
-    filename,
-    lineMap: getLineMapFromPatchString(patch),
-    lintErrors: _.get(eslint.executeOnText(content, filename), 'results[0].messages'),
-    sha,
-    prNumber,
-});
+}
 
 /**
  * Send a comment to Github's commit view
@@ -105,15 +65,15 @@ const lintContent = ({ filename, patch, content, sha, prNumber }) => ({
  * @param  {String} sha      Commit's id
  * @param  {Number} prNumber   Pull request number
  */
-const sendSingleComment = (filename, lineMap, lintError, sha, prNumber) => {
+const sendSingleComment = ({ filename, lineMap, lintError, sha, prNumber }) => {
     const { message, line } = lintError;
     const diffLinePosition = lineMap[line];
     // By testing this, we skip the linting messages related to non-modified lines.
     if (diffLinePosition) {
         // return console.log('Lint error on line:', diffLinePosition, message);
         github.pullRequests.createComment({
-            user: process.env.REPOSITORY_OWNER,
-            repo: process.env.REPOSITORY_NAME,
+            user: env('REPOSITORY_OWNER'),
+            repo: env('REPOSITORY_NAME'),
             number: prNumber,
             body: message,
             commit_id: sha,
@@ -127,14 +87,10 @@ const getKey = (line) => '.' + line; // eslint-disable-line prefer-template
 
 /**
  * Send the comments for all the linting messages, to Github
- * @param  {String} filename File filename
- * @param  {Object} lineMap  The map between file and diff view line numbers
- * @param  {Array} messages  ESLint messages
- * @param  {String} sha      Commit's id
- * @param  {Number} prNumber   Pull request number
+ * @param  {Array} lintErrors  ESLint errors
  */
-const sendComments = ({ filename, lineMap, lintErrors, sha, prNumber }) => {
-    const errorsByLine = lintErrors.reduce((acc, lintError) => {
+function groupLintErrorsByLine({ lintErrors }) {
+    return lintErrors.reduce((acc, lintError) => {
         const { ruleId = 'Eslint', message, line } = lintError;
         const key = getKey(line);
         if (!acc[key]) {
@@ -147,73 +103,120 @@ const sendComments = ({ filename, lineMap, lintErrors, sha, prNumber }) => {
 
         return acc;
     }, {});
+}
 
-    Object.keys(errorsByLine).forEach((line) => (
-        sendSingleComment(filename, lineMap, errorsByLine[line], sha, prNumber)
-    ));
-};
+/**
+ * Lint a raw content passed as a string, then return the linting messages.
+ * @param  {String} filename File filename
+ * @param  {String} patch    Commit's Git patch
+ * @param  {String} content  File content
+ * @return {Object}  Linting messages
+ */
+function lintContent({ filename, patch, content }) {
+    return {
+        filename,
+        lineMap: getLineMapFromPatchString(patch),
+        lintErrors: _.get(eslint.executeOnText(content, filename), 'results[0].messages'),
+    };
+}
+
+function getContent(file, ref) {
+    const { filename, patch } = file;
+
+    return github.repos.getContent({
+        user: env('REPOSITORY_OWNER'),
+        repo: env('REPOSITORY_NAME'),
+        path: filename,
+        ref,
+    }).then((data) => ({
+        filename,
+        patch,
+        content: atob(data.content),
+    }));
+}
 
 function treatPayload(payload) {
     const { number, pull_request } = payload;
+
     github.pullRequests.getFiles({
-        user: process.env.REPOSITORY_OWNER,
-        repo: process.env.REPOSITORY_NAME,
+        user: env('REPOSITORY_OWNER'),
+        repo: env('REPOSITORY_NAME'),
         number,
     }).then((files) => {
         const jsFiles = filterJavascriptFiles(files);
-        jsFiles.forEach((file) => (
-            getContent(file, pull_request.head.ref, number, pull_request.head.sha)
+
+        jsFiles.forEach((file) => {
+            const sendComments = (errorsByLine) => {
+                Object.keys(errorsByLine).forEach((line) => (
+                    sendSingleComment({
+                        filename: file.filename,
+                        lineMap: getLineMapFromPatchString(file.patch),
+                        lintError: errorsByLine[line],
+                        sha: pull_request.head.sha,
+                        prNumber: number,
+                    })
+                ));
+            };
+
+            getContent(file, pull_request.head.ref)
                 .then(lintContent)
-                .then(sendComments))
-        );
+                .then(groupLintErrorsByLine)
+                .then(sendComments);
+        });
     });
 }
 
 // Server
 app.use(bodyParser.json());
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (env('PORT') || 5000));
 
 app.post('/', ({ body: payload }, response) => {
-    if (payload && payload.pull_request && payload.action === 'opened') {
+    const openedOrReopened = payload.action === 'opened' || payload.action === 'reopened';
+    if (payload && payload.pull_request && openedOrReopened) {
+        /* eslint-disable no-console */
+        console.log('A pull request was opened. Starting to lint content..');
+        /* eslint-enable no-console */
         treatPayload(payload);
     }
-    console.log(process.env.GITHUB_USERNAME, ': Received request');
     response.end();
 });
 
 
 const requiredVars = ['GITHUB_USERNAME', 'GITHUB_PASSWORD', 'REPOSITORY_OWNER', 'REPOSITORY_NAME'];
 function isReadyToStart() {
-    const setVars = requiredVars.filter((varName) => process.env[varName]);
-    const stillMissing = requiredVars.filter((varName) => !process.env[varName]);
+    const definedVariables = requiredVars.filter((varName) => env(varName));
+    const stillMissing = requiredVars.filter((varName) => !env(varName));
 
-    const setVarsOutput = setVars.map((varName) => (
+    const definedVars = definedVariables.map((varName) => (
         varName === 'GITHUB_PASSWORD' ?
             `* ${varName} = ********` :
-            `* ${varName} = ${process.env[varName]}`
+            `* ${varName} = ${env(varName)}`
     )).join('\n');
-    console.log(`Set variables:\n${setVarsOutput}`);
+
+    console.log(`Defined variables:\n${definedVars}`); // eslint-disable-line no-console
 
     if (stillMissing.length > 0) {
+        /* eslint-disable no-console */
         console.log('Still waiting for following env vars to be set:', stillMissing.join(', '));
+        /* eslint-enable no-console */
     }
     return stillMissing.length === 0;
 }
 
-function startApp() {
+(function startApp() {
     if (isReadyToStart()) {
         github.authenticate({
             type: 'basic',
-            username: process.env.GITHUB_USERNAME,
-            password: process.env.GITHUB_PASSWORD,
+            username: env('GITHUB_USERNAME'),
+            password: env('GITHUB_PASSWORD'),
         });
         app.listen(app.get('port'), () => {
-            console.log(process.env.GITHUB_USERNAME, 'is running on port', app.get('port'));
+            /* eslint-disable no-console */
+            console.log('ESLint-bot is running on port', app.get('port'));
+            /* eslint-enable no-console */
         });
     } else {
         setTimeout(startApp, 2000);
     }
-}
-
-startApp();
+}());
